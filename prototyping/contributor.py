b16c0170f99a1d791be77021ea99fa5777b3c51a
Lucas David <26874907+lcsdavid@users.noreply.github.com>
2020-06-19 17:22:00 +0200

    ~ Modified data module, now access data via Google Sheets API, instead of downloading '.csv'. + Added personal contributor track ranking computation.

--- prototyping/contributor.py
+++ prototyping/contributor.py
@@ -1,22 +1,29 @@
+import collections
 
 class contributor:
     def __init__(self, name, index):
         self.name = name
         self.index = index
+        self.ratings = collections.defaultdict(list)
         
     def __str__(self) -> str:
         return self.name
         
     def __repr__(self) -> str:
         return self.name
+        
+    def __sort_ratings(self):
+        self.ratings = { r: self.ratings[r] for r in sorted(self.ratings, reverse=True) }
 
+    def personal_ranking(self) -> list:
+        self.__sort_ratings()
+        return self.ratings.values()
 
+# Hardcoded contributors and column indexes.
 CONTRIBUTORS = [ 
-    contributor("Quentin", 5), contributor("Gary", 6),
-    contributor("20100", 7), contributor("Romain", 8),
-    contributor("Samuel", 9), contributor("Galtier", 10), 
-    contributor("Roxane", 11), contributor("Clémence", 12), 
-    contributor("Lucas", 13)
+    contributor('Quentin', 'ntQ'), contributor('Gary', 'ntG'), contributor('Vincent', 'ntV'),
+    contributor('Romain', 'ntR'), contributor('Samuel', 'ntS'), contributor('Galtier', 'ntGl'), 
+    contributor('Roxane', 'ntRx'), contributor('Clémence', 'ntC'), contributor('Lucas', 'ntL')
 ]
 
 if __name__ == "__main__":
--- prototyping/data.py
+++ prototyping/data.py
@@ -1,15 +1,24 @@
-import csv
-import requests
+import pandas
 
-""" Constants to access data on Google Sheets. """
-GOOGLE_SHEETS_URL = "https://docs.google.com/spreadsheets/"
-NOTATIONS_SHEET = "1b75J-QTGrujSgF9r0_JPOKkcXAwzFVwpETOAyVBw8ak/"
-EXPORT_AS_CSV = "export?format=csv&id=1b75J-QTGrujSgF9r0_JPOKkcXAwzFVwpETOAyVBw8ak&gid=0"
-URL = GOOGLE_SHEETS_URL + "d/" + NOTATIONS_SHEET + EXPORT_AS_CSV
+# from oauth2client.service_account import ServiceAccountCredentials 
+from googleapiclient.discovery import build
+from google.oauth2.service_account import Credentials 
 
-""" Returns "Notations" sheets as a csv.reader. """
-def data() -> csv.reader:
-    with requests.Session() as session:
-        return csv.reader(session.get(URL).content.decode("utf-8").splitlines(), delimiter=",")
+CREDENTIALS_PATH_GOOGLE = 'google-credentials.json'
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+SPREADSHEET = '1b75J-QTGrujSgF9r0_JPOKkcXAwzFVwpETOAyVBw8ak'
 
+# Load service account credentials.
+__credentials = Credentials.from_service_account_file(CREDENTIALS_PATH_GOOGLE, scopes=SCOPES)
 
+# Creates Google Sheets API (v4/latest) service.
+service = build('sheets', 'v4', credentials=__credentials)
+# Gets values from Ach! Musik: Notations sheet.
+values = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET, range='Notations').execute()['values']
+headers = values.pop(0)
+# Format data as pandas.DataFrame
+data = pandas.DataFrame(values, columns=headers)
+
+if __name__ == '__main__':
+    print('Ach! Musik notations data:')
+    print(data)
